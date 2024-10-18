d7955689de37af8eaf552508d7d3d11e5a45f3f1
CandationClavel <canda.hdez@gmail.com>
2024-10-17 21:42:04 -0700

    Implementadas HU 11 y 14 (HU 07 puede ser desde /admin)

--- creadorDeProyectos/creadorAPP/models.py
+++ creadorDeProyectos/creadorAPP/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class AmbitoProyecto(models.Model):
+    descripcion_ambito = models.TextField(help_text="Descripción del alcance y expectativas del proyecto.")
+    caracteristicas_principales = models.TextField(help_text="Características principales del proyecto.")
+    limitaciones = models.TextField(help_text="Limitaciones del proyecto.")
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    ultima_actualizacion = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Ámbito definido"
--- creadorDeProyectos/creadorAPP/urls.py
+++ creadorDeProyectos/creadorAPP/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
-    path('protected/', views.ProtectedView.as_view(), name='protected'), 
+    path('protected/', views.ProtectedView.as_view(), name='protected'),
+    path('definir-ambito/', views.definir_ambito_proyecto, name='definir_ambito_proyecto'),
+    path('ver-ambito/', views.ver_ambito_proyecto, name='ver_ambito_proyecto'),
+    # path('proyecto/eliminar/<int:proyecto_id>/', views.eliminar_proyecto, name='eliminar_proyecto'),
 ]
